424a272674ece2b604b1b69277dafda0494a5534
Shrawan Kumar <thakurshrawan992@gmail.com>
2026-06-05 14:00:40 +0530

    Time: 4 ms (11.86%) | Memory: 13.3 MB (65.98%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        if not ops:
+            return m * n
+
+        min_a = m
+        min_b = n
+
+        for a, b in ops:
+            min_a = min(min_a, a)
+            min_b = min(min_b, b)
+
+        return min_a * min_b
+        
