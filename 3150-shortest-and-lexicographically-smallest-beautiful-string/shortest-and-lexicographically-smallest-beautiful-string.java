4920d0be13752d18536e774f315c21e349caf9f7
Shrawan Kumar <thakurshrawan992@gmail.com>
2026-08-26 10:17:16 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (28.86%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+
+        int n = s.length();
+        int[] pos = new int[n];
+        int count = 0;
+
+        // Store positions of all 1's
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                pos[count++] = i;
+            }
+        }
+
+        // Not enough 1's
+        if (count < k) {
+            return "";
+        }
+
+        String ans = "";
+
+        // Check every group of k consecutive 1's
+        for (int i = 0; i + k - 1 < count; i++) {
+
+            int start = pos[i];
+            int end = pos[i + k - 1];
+
+            String current = s.substring(start, end + 1);
+
+            // First substring or shorter substring
+            if (ans.equals("") || current.length() < ans.length()) {
+                ans = current;
+            }
+            // Same length -> lexicographically smaller
+            else if (current.length() == ans.length()
+                    && current.compareTo(ans) < 0) {
+                ans = current;
+            }
+        }
+
+        return ans;
+    }
+}
