106ad8b3fbaff982aeb82ad40618d25c7096dc0e
Shrawan Kumar <thakurshrawan992@gmail.com>
2026-09-07 21:23:22 +0530

    Time: 5 ms (54.61%) | Memory: 43.6 MB (31.56%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        int MOD = 1000000007;
+
+        long[] dp = new long[s.length() + 1];
+        long[] last = new long[26];
+
+        dp[0] = 1; // empty subsequence
+
+        for (int i = 1; i <= s.length(); i++) {
+            int c = s.charAt(i - 1) - 'a';
+
+            dp[i] = (2 * dp[i - 1]) % MOD;
+
+            dp[i] = (dp[i] - last[c] + MOD) % MOD;
+
+            last[c] = dp[i - 1];
+        }
+
+        // Remove empty subsequence
+        return (int)((dp[s.length()] - 1 + MOD) % MOD);
+    }
+}
